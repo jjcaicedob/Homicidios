c3d475e1b01c19e146a845a8ff527546690e4637
John Caicedo <github@johncaicedo.com.co>
2025-05-23 10:43:50 -0500

    Update dim_fechas_create.sql

--- DataWareHouse/Scripts/dim_fechas_create.sql
+++ DataWareHouse/Scripts/dim_fechas_create.sql
@@ -1,4 +1,4 @@
-- DataWareHouse.dbo.DIM_Fechas definition
+-- DataWareHouse.dbo.DIM_Fechas definition
 
 -- Drop table
 
@@ -7,9 +7,10 @@
 CREATE TABLE DataWareHouse.dbo.DIM_Fechas (
 	FechaHecho date NOT NULL,
 	Annio int NOT NULL,
-  Mes tinyint NOT NULL,	
+  	Mes tinyint NOT NULL,	
 	Dia tinyint NOT NULL,
 	DiaSemana tinyint NOT NULL,
 	Semana tinyint NOT NULL,
+	Trimestre tinyint NOT NULL,
 	CONSTRAINT DIM_Fechas_PK PRIMARY KEY (FechaHecho)
 );
