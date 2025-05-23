104ea5fc9b17c26a01921b0bfbe1c0a41ddbbd2b
John Caicedo <github@johncaicedo.com.co>
2025-05-23 10:25:15 -0500

    Update dim_departamentos_create.sql

--- DataWareHouse/Scripts/dim_departamentos_create.sql
+++ DataWareHouse/Scripts/dim_departamentos_create.sql
@@ -1,10 +1,10 @@
--- DWH.dbo.DIM_Departamentos definition
+-- DataWareHouse.dbo.DIM_Departamentos definition
 
 -- Drop table
 
--- DROP TABLE DWH.dbo.DIM_Departamentos;
+-- DROP TABLE DataWareHouse.dbo.DIM_Departamentos;
 
-CREATE TABLE DWH.dbo.DIM_Departamentos (
+CREATE TABLE DataWareHouse.dbo.DIM_Departamentos (
 	CodDpto tinyint NOT NULL,
 	NomDpto nvarchar(60) NOT NULL,
 	Latitud float NOT NULL,
