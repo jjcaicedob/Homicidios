1ec7e41dcd59f07fc7206e05073641195d331352
John Caicedo <github@johncaicedo.com.co>
2025-05-23 15:26:20 -0500

    Update dim_trimestres_create.sql

--- DataWareHouse/Scripts/dim_trimestres_create.sql
+++ DataWareHouse/Scripts/dim_trimestres_create.sql
@@ -6,7 +6,7 @@
 
 CREATE TABLE DataWareHouse.dbo.DIM_Trimestres (
 	Trimestre tinyint IDENTITY(1,1) NOT NULL,
-	NomTrimestre nvarchar(15) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	NomCortoTrimestre char(6) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	NomTrimestre nvarchar(15) NOT NULL,
+	NomCortoTrimestre char(6) NOT NULL,
 	CONSTRAINT DIM_Trimestres_PK PRIMARY KEY (Trimestre)
 );
