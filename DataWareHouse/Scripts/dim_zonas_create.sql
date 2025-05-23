7404b3afbca86b35cc8432dc5e0868c368e633c3
John Caicedo <github@johncaicedo.com.co>
2025-05-23 10:27:50 -0500

    Update dim_zonas_create.sql

--- DataWareHouse/Scripts/dim_zonas_create.sql
+++ DataWareHouse/Scripts/dim_zonas_create.sql
@@ -1,11 +1,11 @@
--- DWH.dbo.DIM_Zonas definition
+-- DataWareHouse.dbo.DIM_Zonas definition
 
 -- Drop table
 
--- DROP TABLE DWH.dbo.DIM_Zonas;
+-- DROP TABLE DataWareHouse.dbo.DIM_Zonas;
 
-CREATE TABLE DWH.dbo.DIM_Zonas (
+CREATE TABLE DataWareHouse.dbo.DIM_Zonas (
 	IdZona tinyint IDENTITY(1,1) NOT NULL,
-  NomZona nvarchar(10) NOT NULL,	
+  	NomZona nvarchar(10) NOT NULL,	
 	CONSTRAINT DIM_Zonas_PK PRIMARY KEY (IdZona)
 );
