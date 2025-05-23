f423a7f12e57244221ddd9796443d18a181e9e27
John Caicedo <github@johncaicedo.com.co>
2025-05-23 15:26:50 -0500

    Update dim_dias_semana_create.sql

--- DataWareHouse/Scripts/dim_dias_semana_create.sql
+++ DataWareHouse/Scripts/dim_dias_semana_create.sql
@@ -6,7 +6,7 @@
 
 CREATE TABLE DataWareHouse.dbo.DIM_Dias_Semana (
 	DiaSemana tinyint IDENTITY(1,1) NOT NULL,
-	NomDiaSemana nvarchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	NomCortoDiaSemana char(3) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	NomDiaSemana nvarchar(10) NOT NULL,
+	NomCortoDiaSemana char(3) NOT NULL,
 	CONSTRAINT DIM_Dias_Semana_PK PRIMARY KEY (DiaSemana)
 );
