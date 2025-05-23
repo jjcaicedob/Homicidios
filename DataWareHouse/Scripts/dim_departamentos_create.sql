b8b8100c4d9c4bccc5a05e9f02c955f08eac59c0
John Caicedo <github@johncaicedo.com.co>
2025-05-23 10:24:14 -0500

    Update dim_departamentos_create.sql

--- DataWareHouse/Scripts/dim_departamentos_create.sql
+++ DataWareHouse/Scripts/dim_departamentos_create.sql
@@ -5,9 +5,9 @@
 -- DROP TABLE DWH.dbo.DIM_Departamentos;
 
 CREATE TABLE DWH.dbo.DIM_Departamentos (
-	COD_DPTO int NOT NULL,
-	NOM_DPTO nvarchar(60) NOT NULL,
-	LATITUD float NOT NULL,
-	LONGITUD float NOT NULL,
-	CONSTRAINT DIM_Departamentos_PK PRIMARY KEY (COD_DPTO)
+	CodDpto tinyint NOT NULL,
+	NomDpto nvarchar(60) NOT NULL,
+	Latitud float NOT NULL,
+	Longitud float NOT NULL,
+	CONSTRAINT DIM_Departamentos_PK PRIMARY KEY (CodDpto)
 );
