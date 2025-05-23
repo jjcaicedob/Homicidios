8a03cb1164b911d82390903a3f10cf8babfd7e98
John Caicedo <github@johncaicedo.com.co>
2025-05-23 09:59:20 -0500

    Create dl_homicidios_create.sql

---
+++ DataLake/Scripts/dl_homicidios_create.sql
@@ -0,0 +1,16 @@
+-- DataLake.dbo.DL_Homicidios definition
+
+-- Drop table
+
+-- DROP TABLE DataLake.dbo.DL_Homicidios;
+
+CREATE TABLE DataLake.dbo.DL_Homicidios (
+	FECHA_HECHO date NULL,
+	COD_DEPTO tinyint NULL,
+	DEPARTAMENTO nvarchar(60) NULL,
+	COD_MUNI int NULL,
+	MUNICIPIO nvarchar(30) NULL,
+	ZONA nvarchar(10) NULL,
+	SEXO nvarchar(20) NULL,
+	CANTIDAD smallint NULL
+);
