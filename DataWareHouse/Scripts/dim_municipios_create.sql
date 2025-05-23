cb129ada7bca54be7fc06988fba09584a65b49ed
John Caicedo <github@johncaicedo.com.co>
2025-05-23 10:27:00 -0500

    Update dim_municipios_create.sql

--- DataWareHouse/Scripts/dim_municipios_create.sql
+++ DataWareHouse/Scripts/dim_municipios_create.sql
@@ -1,15 +1,15 @@
--- DWH.dbo.DIM_Municipios definition
+-- DataWareHouse.dbo.DIM_Municipios definition
 
 -- Drop table
 
--- DROP TABLE DWH.dbo.DIM_Municipios;
+-- DROP TABLE DataWareHouse.dbo.DIM_Municipios;
 
-CREATE TABLE DWH.dbo.DIM_Municipios (
-	COD_MPIO int NOT NULL,
-	NOM_MPIO nvarchar(30) NOT NULL,
-	LATITUD float NOT NULL,
-	LONGITUD float NOT NULL,
-	COD_DPTO tinyint NOT NULL,
-	CONSTRAINT DIM_Municipios_PK PRIMARY KEY (COD_MPIO),
-	CONSTRAINT DIM_Municipios_DIM_Departamentos_FK FOREIGN KEY (COD_DPTO) REFERENCES DWH.dbo.DIM_Departamentos(COD_DPTO)
+CREATE TABLE DataWareHouse.dbo.DIM_Municipios (
+	CodMpio int NOT NULL,
+	CodDpto tinyint NOT NULL,	
+	NomMpio nvarchar(30) NOT NULL,
+	Latitud float NOT NULL,
+	Longitud float NOT NULL,
+	CONSTRAINT DIM_Municipios_PK PRIMARY KEY (CodMpio),
+	CONSTRAINT DIM_Municipios_DIM_Departamentos_FK FOREIGN KEY (CodDpto) REFERENCES DataWareHouse.dbo.DIM_Departamentos(CodDpto)
 );
