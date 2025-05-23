08bfa3b763bbe97b6c7b2780351d6efec4c06955
John Caicedo <github@johncaicedo.com.co>
2025-05-23 09:35:04 -0500

    Update dl_departamentos_create.sql

--- DataLake/Scripts/dl_departamentos_create.sql
+++ DataLake/Scripts/dl_departamentos_create.sql
@@ -6,8 +6,8 @@
 
 CREATE TABLE DataLake.dbo.DL_Departamentos (
 	COD_DPTO tinyint NULL,
-	NOM_DPTO nvarchar(60) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	NOM_DPTO nvarchar(60) NULL,
 	LATITUD float NULL,
 	LONGITUD float NULL,
-	GeoDepartamento varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	GeoDepartamento varchar(50) NULL
 );
