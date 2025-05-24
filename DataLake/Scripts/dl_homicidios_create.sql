f2771f8254ee9cec92a1f5e7305df547a1f861cc
John Caicedo <github@johncaicedo.com.co>
2025-05-24 18:47:00 -0500

    Update dl_homicidios_create.sql

--- DataLake/Scripts/dl_homicidios_create.sql
+++ DataLake/Scripts/dl_homicidios_create.sql
@@ -1,16 +1,17 @@
--- DataLake.dbo.DL_Homicidios definition
+-- Explicación:
+-- OBJECT_ID('dbo.DL_Homicidios', 'U'): Verifica si existe un objeto de tipo tabla ('U' para "User Table") con ese nombre.
+-- Si OBJECT_ID devuelve NULL, la tabla no existe, por lo que se ejecuta la instrucción CREATE TABLE.
 
--- Drop table
-
--- DROP TABLE DataLake.dbo.DL_Homicidios;
-
-CREATE TABLE DataLake.dbo.DL_Homicidios (
-	FECHA_HECHO date NULL,
-	COD_DEPTO tinyint NULL,
-	DEPARTAMENTO nvarchar(60) NULL,
-	COD_MUNI int NULL,
-	MUNICIPIO nvarchar(30) NULL,
-	ZONA nvarchar(10) NULL,
-	SEXO nvarchar(20) NULL,
-	CANTIDAD tinyint NULL
-);
+IF OBJECT_ID('dbo.DL_Homicidios', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.DL_Homicidios (
+		FECHA_HECHO date NULL,
+		COD_DEPTO tinyint NULL,
+		DEPARTAMENTO nvarchar(60) NULL,
+		COD_MUNI int NULL,
+		MUNICIPIO nvarchar(30) NULL,
+		ZONA nvarchar(10) NULL,
+		SEXO nvarchar(20) NULL,
+		CANTIDAD tinyint NULL
+	);
+END;
