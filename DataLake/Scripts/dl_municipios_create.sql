5d4423a17decffb685413f72441ca872bd47ff39
John Caicedo <github@johncaicedo.com.co>
2025-05-24 18:35:46 -0500

    Update dl_municipios_create.sql

--- DataLake/Scripts/dl_municipios_create.sql
+++ DataLake/Scripts/dl_municipios_create.sql
@@ -1,16 +1,17 @@
--- DataLake.dbo.DL_Municipios definition
+-- Explicación:
+-- OBJECT_ID('dbo.DL_Municipios', 'U'): Verifica si existe un objeto de tipo tabla ('U' para "User Table") con ese nombre.
+-- Si OBJECT_ID devuelve NULL, la tabla no existe, por lo que se ejecuta la instrucción CREATE TABLE.
 
--- Drop table
-
--- DROP TABLE DataLake.dbo.DL_Municipios;
-
-CREATE TABLE DataLake.dbo.DL_Municipios (
-	COD_DPTO tinyint NULL,
-	NOM_DPTO nvarchar(60) NULL,
-	COD_MPIO int NULL,
-	NOM_MPIO nvarchar(30) NULL,
-	TIPO nvarchar(25) NULL,
-	LATITUD float NULL,
-	LONGITUD float NULL,
-	GeoMunicipio nvarchar(40) NULL
-);
+IF OBJECT_ID('dbo.DL_Municipios', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.DL_Municipios (
+		COD_DPTO tinyint NULL,
+		NOM_DPTO nvarchar(60) NULL,
+		COD_MPIO int NULL,
+		NOM_MPIO nvarchar(30) NULL,
+		TIPO nvarchar(25) NULL,
+		LATITUD float NULL,
+		LONGITUD float NULL,
+		GeoMunicipio nvarchar(40) NULL
+	);
+END;
