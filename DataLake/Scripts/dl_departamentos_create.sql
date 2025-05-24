c693ad88d95e4cb0a623398af653d973bb79bbbe
John Caicedo <github@johncaicedo.com.co>
2025-05-24 17:40:20 -0500

    Update dl_departamentos_create.sql

--- DataLake/Scripts/dl_departamentos_create.sql
+++ DataLake/Scripts/dl_departamentos_create.sql
@@ -1,13 +1,14 @@
--- DataLake.dbo.DL_Departamentos definition
+-- Explicación:
+-- OBJECT_ID('dbo.DL_Departamentos', 'U'): Verifica si existe un objeto de tipo tabla ('U' para "User Table") con ese nombre.
+-- Si OBJECT_ID devuelve NULL, la tabla no existe, por lo que se ejecuta la instrucción CREATE TABLE.
 
--- Drop table
-
--- DROP TABLE DataLake.dbo.DL_Departamentos;
-
-CREATE TABLE DataLake.dbo.DL_Departamentos (
-	COD_DPTO tinyint NULL,
-	NOM_DPTO nvarchar(60) NULL,
-	LATITUD float NULL,
-	LONGITUD float NULL,
-	GeoDepartamento nvarchar(35) NULL
-);
+IF OBJECT_ID('dbo.DL_Departamentos', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.DL_Departamentos (
+		COD_DPTO tinyint NULL,
+		NOM_DPTO nvarchar(60) NULL,
+		LATITUD float NULL,
+		LONGITUD float NULL,
+		GeoDepartamento nvarchar(35) NULL
+	);
+END;
