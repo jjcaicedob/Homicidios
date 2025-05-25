50ff0979f07a46644c20c7b41acb7d7bb10369ea
John Caicedo <github@johncaicedo.com.co>
2025-05-25 11:03:39 -0500

    Update dim_departamentos_create.sql

--- DataWareHouse/Scripts/dim_departamentos_create.sql
+++ DataWareHouse/Scripts/dim_departamentos_create.sql
@@ -1,13 +1,14 @@
--- DataWareHouse.dbo.DIM_Departamentos definition
+--- Explicación:
+-- OBJECT_ID('dbo.DIM_Departamentos', 'U'): Verifica si existe un objeto de tipo tabla ('U' para "User Table") con ese nombre.
+-- Si OBJECT_ID devuelve NULL, la tabla no existe, por lo que se ejecuta la instrucción CREATE TABLE.
 
--- Drop table
-
--- DROP TABLE DataWareHouse.dbo.DIM_Departamentos;
-
-CREATE TABLE DataWareHouse.dbo.DIM_Departamentos (
-	CodDpto tinyint NOT NULL,
-	NomDpto nvarchar(60) NOT NULL,
-	Latitud float NOT NULL,
-	Longitud float NOT NULL,
-	CONSTRAINT DIM_Departamentos_PK PRIMARY KEY (CodDpto)
-);
+IF OBJECT_ID('dbo.DIM_Departamentos', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.DIM_Departamentos (
+		CodDpto tinyint NOT NULL,
+		NomDpto nvarchar(60) NOT NULL,
+		Latitud float NOT NULL,
+		Longitud float NOT NULL,
+		CONSTRAINT DIM_Departamentos_PK PRIMARY KEY (CodDpto)
+	);
+END;
