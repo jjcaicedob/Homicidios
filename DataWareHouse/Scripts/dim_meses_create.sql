a14bc2ae91b2bdd5703f847e3bfea8c4caa5aeb3
John Caicedo <github@johncaicedo.com.co>
2025-05-25 18:07:26 -0500

    Update dim_meses_create.sql

--- DataWareHouse/Scripts/dim_meses_create.sql
+++ DataWareHouse/Scripts/dim_meses_create.sql
@@ -1,12 +1,13 @@
--- DataWareHouse.dbo.DIM_Meses definition
+-- Explicación:
+-- OBJECT_ID('dbo.DIM_Meses', 'U'): Verifica si existe un objeto de tipo tabla ('U' para "User Table") con ese nombre.
+-- Si OBJECT_ID devuelve NULL, la tabla no existe, por lo que se ejecuta la instrucción CREATE TABLE.
 
--- Drop table
-
--- DROP TABLE DataWareHouse.dbo.DIM_Meses;
-
-CREATE TABLE DataWareHouse.dbo.DIM_Meses (
-	Mes tinyint IDENTITY(1,1) NOT NULL,
-	NomMes nvarchar(10) NOT NULL,
-	NomCortoMes char(3) NOT NULL,
-	CONSTRAINT DIM_Meses_PK PRIMARY KEY (Mes)
-);
+IF OBJECT_ID('dbo.DIM_Meses', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.DIM_Meses (
+		Mes tinyint IDENTITY(1,1) NOT NULL,
+		NomMes nvarchar(10) NOT NULL,
+		NomCortoMes char(3) NOT NULL,
+		CONSTRAINT DIM_Meses_PK PRIMARY KEY (Mes)
+	);
+END;
