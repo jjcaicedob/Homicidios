78dd13ef1f31da2ae2098a8dd218b1281e8c6c2b
John Caicedo <github@johncaicedo.com.co>
2025-05-25 17:10:19 -0500

    Update dim_dias_semana_create.sql

--- DataWareHouse/Scripts/dim_dias_semana_create.sql
+++ DataWareHouse/Scripts/dim_dias_semana_create.sql
@@ -1,12 +1,13 @@
--- DataWareHouse.dbo.DIM_Dias_Semana definition
+-- Explicación:
+-- OBJECT_ID('dbo.DIM_Dias_Semana', 'U'): Verifica si existe un objeto de tipo tabla ('U' para "User Table") con ese nombre.
+-- Si OBJECT_ID devuelve NULL, la tabla no existe, por lo que se ejecuta la instrucción CREATE TABLE.
 
--- Drop table
-
--- DROP TABLE DataWareHouse.dbo.DIM_Dias_Semana;
-
-CREATE TABLE DataWareHouse.dbo.DIM_Dias_Semana (
-	DiaSemana tinyint IDENTITY(1,1) NOT NULL,
-	NomDiaSemana nvarchar(10) NOT NULL,
-	NomCortoDiaSemana char(3) NOT NULL,
-	CONSTRAINT DIM_Dias_Semana_PK PRIMARY KEY (DiaSemana)
-);
+IF OBJECT_ID('dbo.DIM_Dias_Semana', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.DIM_Dias_Semana (
+		DiaSemana tinyint IDENTITY(1,1) NOT NULL,
+		NomDiaSemana nvarchar(10) NOT NULL,
+		NomCortoDiaSemana char(3) NOT NULL,
+		CONSTRAINT DIM_Dias_Semana_PK PRIMARY KEY (DiaSemana)
+	);
+END;
