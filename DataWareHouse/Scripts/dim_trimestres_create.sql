236b2b1ca5ae25fe2601dfb6ff96d4428c0f564a
John Caicedo <github@johncaicedo.com.co>
2025-05-25 18:22:02 -0500

    Update dim_trimestres_create.sql

--- DataWareHouse/Scripts/dim_trimestres_create.sql
+++ DataWareHouse/Scripts/dim_trimestres_create.sql
@@ -1,12 +1,13 @@
--- DataWareHouse.dbo.DIM_Trimestres definition
+-- Explicación:
+-- OBJECT_ID('dbo.DIM_Trimestres', 'U'): Verifica si existe un objeto de tipo tabla ('U' para "User Table") con ese nombre.
+-- Si OBJECT_ID devuelve NULL, la tabla no existe, por lo que se ejecuta la instrucción CREATE TABLE.
 
--- Drop table
-
--- DROP TABLE DataWareHouse.dbo.DIM_Trimestres;
-
-CREATE TABLE DataWareHouse.dbo.DIM_Trimestres (
-	Trimestre tinyint IDENTITY(1,1) NOT NULL,
-	NomTrimestre nvarchar(15) NOT NULL,
-	NomCortoTrimestre char(6) NOT NULL,
-	CONSTRAINT DIM_Trimestres_PK PRIMARY KEY (Trimestre)
-);
+IF OBJECT_ID('dbo.DIM_Trimestres', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.DIM_Trimestres (
+		Trimestre tinyint IDENTITY(1,1) NOT NULL,
+		NomTrimestre nvarchar(15) NOT NULL,
+		NomCortoTrimestre char(6) NOT NULL,
+		CONSTRAINT DIM_Trimestres_PK PRIMARY KEY (Trimestre)
+	);
+END;
