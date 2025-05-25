eaecf0e48ef5f1622a4d95d5f1f9850e19c2c6c8
John Caicedo <github@johncaicedo.com.co>
2025-05-25 15:05:44 -0500

    Update dim_departamentos_insert.sql

--- DataWareHouse/Scripts/dim_departamentos_insert.sql
+++ DataWareHouse/Scripts/dim_departamentos_insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO DataWareHouse.dbo.DIM_Departamentos
+INSERT INTO dbo.DIM_Departamentos
 (CodDpto, NomDpto, Latitud, Longitud)
 SELECT COD_DPTO, NOM_DPTO, LATITUD, LONGITUD
 FROM DataLake.dbo.DL_Departamentos
