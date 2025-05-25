498917152eda4c58808ecc3b053acaca3e595f8a
John Caicedo <github@johncaicedo.com.co>
2025-05-25 11:39:57 -0500

    Update dim_municipios_create.sql

--- DataWareHouse/Scripts/dim_municipios_create.sql
+++ DataWareHouse/Scripts/dim_municipios_create.sql
@@ -1,15 +1,16 @@
--- DataWareHouse.dbo.DIM_Municipios definition
+-- Explicación:
+-- OBJECT_ID('dbo.DIM_Municipios', 'U'): Verifica si existe un objeto de tipo tabla ('U' para "User Table") con ese nombre.
+-- Si OBJECT_ID devuelve NULL, la tabla no existe, por lo que se ejecuta la instrucción CREATE TABLE.
 
--- Drop table
-
--- DROP TABLE DataWareHouse.dbo.DIM_Municipios;
-
-CREATE TABLE DataWareHouse.dbo.DIM_Municipios (
-	CodMpio int NOT NULL,
-	CodDpto tinyint NOT NULL,	
-	NomMpio nvarchar(30) NOT NULL,
-	Latitud float NOT NULL,
-	Longitud float NOT NULL,
-	CONSTRAINT DIM_Municipios_PK PRIMARY KEY (CodMpio),
-	CONSTRAINT DIM_Municipios_DIM_Departamentos_FK FOREIGN KEY (CodDpto) REFERENCES DataWareHouse.dbo.DIM_Departamentos(CodDpto)
-);
+IF OBJECT_ID('dbo.DIM_Municipios', 'U') IS NULL
+BEGIN
+	CREATE TABLE dbo.DIM_Municipios (
+		CodMpio int NOT NULL,
+		CodDpto tinyint NOT NULL,	
+		NomMpio nvarchar(30) NOT NULL,
+		Latitud float NOT NULL,
+		Longitud float NOT NULL,
+		CONSTRAINT DIM_Municipios_PK PRIMARY KEY (CodMpio),
+		CONSTRAINT DIM_Municipios_DIM_Departamentos_FK FOREIGN KEY (CodDpto) REFERENCES dbo.DIM_Departamentos(CodDpto)
+	);
+END;
